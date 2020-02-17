b85369c3268e0273f79f22721b9647b2b2850b20
上大田孝 <takashi@ueootakou-no-MacBook-Air-6.local>
2020-02-17 21:26:34 +0900

    binaryファイル追加 CSVデータと動線分析結果追加 GimbalBeacon購入方法追加 設定ファイル追加

---
+++ M5StackBeaconLogger/load.bat
@@ -0,0 +1,9 @@
+set PORT=COM3
+set APP=M5StackBeaconLogger
+
+.\esptool.exe --port %PORT% --baud 921600 ^
+     --chip esp32  --before default_reset --after hard_reset ^
+     write_flash -z --flash_mode dio --flash_freq 80m --flash_size detect ^
+     0x10000 ./%APP%.ino.m5stack_core_esp32.bin
+
+pause
